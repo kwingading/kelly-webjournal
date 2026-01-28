a117e5c1fb6b6be522c2f1eb5c449fb0124a860f
Kelly W <35130754+kwingading@users.noreply.github.com>
2026-01-28 21:37:19 +0800

    Update vite.config.js - change base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/kelly-webjournal/', 
+  base: '/', 
   plugins: [react(), tailwindcss()],
 })
